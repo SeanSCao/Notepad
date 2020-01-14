efad0fd9b9adc28f4e09dddeea758ebd71fe961a
SeanSCao <ssc543@nyu.edu>
2020-01-13 20:25:07 -0500

    added fontawesome

--- src/components/Client/Dictionary/item.js
+++ src/components/Client/Dictionary/item.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 
 const ItemList = ({ items, onRemoveItem, authUser }) => (
     <div className="card-columns">
@@ -10,14 +12,14 @@ const ItemList = ({ items, onRemoveItem, authUser }) => (
 
 const Item = ({ item, onRemoveItem, authUser }) => (
     <div className="card" style={{ width: "18rem" }}>
+        <div className="d-flex justify-content-end">
+            <FontAwesomeIcon icon={faTrashAlt} onClick={() => onRemoveItem(authUser.uid, item.uid)} className="ml-auto" />
+        </div>
         <div className="card-body">
             <h5 className="card-title">{item.title}</h5>
             <h6 className="card-subtitle mb-2 text-muted">Test</h6>
             <p className="card-text">{item.desc}</p>
         </div>
-        <button type="button" onClick={() => onRemoveItem(authUser.uid, item.uid)}>
-            Delete
-        </button>
     </div >
 );
 
